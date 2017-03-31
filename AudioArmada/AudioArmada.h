336158297562746e75bd191e69ded658edfe5a0d
Corey Walo <cjwalo@gmail.com>
2017-03-31 15:55:16 -0500

    Initial Commit

---
+++ AudioArmada/AudioArmada.h
@@ -0,0 +1,19 @@
+//
+//  AudioArmada.h
+//  AudioArmada
+//
+//  Created by Corey Walo on 3/31/17.
+//  Copyright © 2017 Corey Walo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AudioArmada.
+FOUNDATION_EXPORT double AudioArmadaVersionNumber;
+
+//! Project version string for AudioArmada.
+FOUNDATION_EXPORT const unsigned char AudioArmadaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioArmada/PublicHeader.h>
+
+
